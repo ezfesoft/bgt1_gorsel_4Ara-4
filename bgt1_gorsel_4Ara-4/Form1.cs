ef3675bc6d2112097f42d18c9e8dd793080a5870
slymn_LAB2 <ezfesoftware@gmail.com>
2024-12-04 11:29:28 +0300

    Proje dosyası ekle.

---
+++ bgt1_gorsel_4Ara-4/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bgt1_gorsel_4Ara_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string meyve;
+            meyve = comboBox1.Text;
+
+            int siraNumarasi;
+            siraNumarasi = comboBox1.SelectedIndex;
+
+            MessageBox.Show(siraNumarasi.ToString());
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label1.Text = comboBox1.Text;
+        }
+    }
+}
